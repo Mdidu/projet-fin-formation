e7e499577969870cf9e3dd7e0ba6134b1bfeb5c9
Mdidu <alexandre.meddas@gmail.com>
2020-05-05 11:18:56 +0200

    commit projet-fin-formation canActivate

---
+++ src/app/services/auth-guard/auth-guard.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot} from '@angular/router';
+import {Observable} from 'rxjs';
+import {AuthService} from '../auth.service';
+
+// Injectable sert à injecter un service dans un autre service !
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuardService implements CanActivate {
+
+  constructor(private authService: AuthService, private router: Router) { }
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean> | boolean {
+    if (this.authService.currentUser) {
+      return true;
+    } else {
+      this.router.navigate(['auth/signin']);
+    }
+  }
+}
